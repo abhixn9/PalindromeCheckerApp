c1eda40bb255d0450a0f8752b5da8b9e5d4c3b5d
abhixn9 <abhixin79@gmail.com>
2026-02-25 11:00:06 +0530

    hello palindrome

--- src/UseCasepalindromeCheckerApp.java
+++ src/UseCasepalindromeCheckerApp.java
@@ -1,5 +1,7 @@
 public class UseCasepalindromeCheckerApp {
    public  static void main(String[] args) {
-       System.out.println("Hello Palindrome");
+       System.out.println("Welcome to the Palindrome Checker Management System");
+       System.out.println("version: 1.0");
+       System.out.println("System initialized successfully");
     }
 }
