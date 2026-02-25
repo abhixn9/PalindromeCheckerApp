ae0d48d4646f3063df83834b1022f382562e8d73
abhixn9 <abhixin79@gmail.com>
2026-02-25 11:04:20 +0530

    UC1

--- src/UseCasepalindromeCheckerApp.java
+++ src/UseCasepalindromeCheckerApp.java
@@ -1,5 +1,7 @@
 public class UseCasepalindromeCheckerApp {
    public  static void main(String[] args) {
-       System.out.println("Hello Palindrome");
+      system.out.println("Welcome to the Palindrome Checker Management System");
+      system.out.println("Version : 1.0");
+      system.out.println("System initialized successfully.");
     }
 }
