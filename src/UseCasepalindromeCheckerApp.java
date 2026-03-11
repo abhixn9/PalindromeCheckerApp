6ea80f15762d9bd98a48da47728705c186ddd2ed
abhi <abhi@hareeshs-MacBook-Air-1758.local>
2026-03-12 00:39:30 +0530

    feature/UC13

--- src/UseCasepalindromeCheckerApp.java
+++ src/UseCasepalindromeCheckerApp.java
@@ -3,82 +3,77 @@ import java.util.Stack;
 import java.util.Deque;
 import java.util.LinkedList;
 
-interface PalindromeStrategy {
-    boolean isPalindrome(String input);
-}
+public class UseCase13PalindromeCheckerApp {
 
-class StackStrategy implements PalindromeStrategy {
-    @Override
-    public boolean isPalindrome(String input) {
+    public static boolean hardcodedCheck(String str) {
+        return str.equals(new StringBuilder(str).toString());
+    }
+
+    public static boolean reverseLoopCheck(String str) {
+        String reversed = "";
+        for (int i = str.length() - 1; i >= 0; i--) {
+            reversed += str.charAt(i);
+        }
+        return str.equals(reversed);
+    }
+
+    public static boolean twoPointerCheck(String str) {
+        char[] arr = str.toCharArray();
+        int start = 0, end = arr.length - 1;
+        while (start < end) {
+            if (arr[start] != arr[end]) return false;
+            start++;
+            end--;
+        }
+        return true;
+    }
+
+    public static boolean stackCheck(String str) {
         Stack<Character> stack = new Stack<>();
-        for (char ch : input.toCharArray()) {
-            stack.push(ch);
-        }
-
-        for (char ch : input.toCharArray()) {
-            if (ch != stack.pop()) return false;
-        }
+        for (char ch : str.toCharArray()) stack.push(ch);
+        for (char ch : str.toCharArray()) if (ch != stack.pop()) return false;
         return true;
     }
-}
 
-class DequeStrategy implements PalindromeStrategy {
-    @Override
-    public boolean isPalindrome(String input) {
+    public static boolean dequeCheck(String str) {
         Deque<Character> deque = new LinkedList<>();
-        for (char ch : input.toCharArray()) {
-            deque.addLast(ch);
-        }
-
-        while (deque.size() > 1) {
-            if (deque.removeFirst() != deque.removeLast()) return false;
-        }
+        for (char ch : str.toCharArray()) deque.addLast(ch);
+        while (deque.size() > 1) if (deque.removeFirst() != deque.removeLast()) return false;
         return true;
     }
-}
-
-class PalindromeCheckerContext {
-    private PalindromeStrategy strategy;
-
-    public PalindromeCheckerContext(PalindromeStrategy strategy) {
-        this.strategy = strategy;
-    }
-
-    public void setStrategy(PalindromeStrategy strategy) {
-        this.strategy = strategy;
-    }
-
-    public boolean check(String input) {
-        return strategy.isPalindrome(input);
-    }
-}
-
-public class UseCase12PalindromeCheckerApp {
 
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
-
-        System.out.println("Enter a string:");
+        System.out.println("Enter a string to test performance:");
         String input = sc.nextLine();
 
-        System.out.println("Choose strategy: 1 = Stack, 2 = Deque");
-        int choice = sc.nextInt();
+        long startTime, endTime;
 
-        PalindromeStrategy strategy;
-        if (choice == 1) {
-            strategy = new StackStrategy();
-        } else {
-            strategy = new DequeStrategy();
-        }
+        startTime = System.nanoTime();
+        hardcodedCheck(input);
+        endTime = System.nanoTime();
+        System.out.println("Hardcoded check time: " + (endTime - startTime) + " ns");
 
-        PalindromeCheckerContext checker = new PalindromeCheckerContext(strategy);
+        startTime = System.nanoTime();
+        reverseLoopCheck(input);
+        endTime = System.nanoTime();
+        System.out.println("Reverse loop check time: " + (endTime - startTime) + " ns");
 
-        if (checker.check(input)) {
-            System.out.println("The given string is a Palindrome (using selected strategy)");
-        } else {
-            System.out.println("The given string is NOT a Palindrome (using selected strategy)");
-        }
+        startTime = System.nanoTime();
+        twoPointerCheck(input);
+        endTime = System.nanoTime();
+        System.out.println("Two-pointer char array check time: " + (endTime - startTime) + " ns");
+
+        startTime = System.nanoTime();
+        stackCheck(input);
+        endTime = System.nanoTime();
+        System.out.println("Stack-based check time: " + (endTime - startTime) + " ns");
+
+        startTime = System.nanoTime();
+        dequeCheck(input);
+        endTime = System.nanoTime();
+        System.out.println("Deque-based check time: " + (endTime - startTime) + " ns");
 
         sc.close();
     }
